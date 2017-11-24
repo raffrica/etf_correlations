f9d34e060bc5380e0a1ba44cfa16fb5ae11f4597
danraff <d.raff@alumni.ubc.ca>
2017-11-23 19:20:27 -0800

    Generate figure of which NAV year to choose

---
+++ src/ishares_metadata_figure_generator_nav_values_not_missing.R
@@ -0,0 +1,48 @@
+#! /usr/bin/env Rscript
+# ishares_metadata_figure_generator_nav_values_not_missing.R
+# Daniel Raff Nov, 2017
+
+# This script reads in data from results/ishares_nav_values_not_missing.csv, 
+# and generates a plot to show the amount of data lost. 
+
+# Usage: Rscript ishares_metadata_figure_generator_nav_values_not_missing.R
+
+library(tidyverse)
+library(forcats)
+
+nav_values_remaining <- read_csv("results/ishares_nav_values_not_missing.csv")
+
+# Remove total from time_elapse variable, as redundant when visualizing
+# proprtion of total 
+nav_values_remaining <- nav_values_remaining[!nav_values_remaining$time_elapse == "total",]
+
+# Convert time_elapse to factor for better visualization
+# and order levels so they are increasing in duration
+nav_values_remaining$time_elapse <- fct_inorder(nav_values_remaining$time_elapse)
+ 
+   #"#ECCBAE"        
+
+# I'd like to choose the NAV with 3 years of data, as I think ~75% of the ETFs
+# is enough data to work with, and 3 years will mean better quality results.  
+
+# This vector will communicate which time_elapse I want to choose
+area_fill_select_3 <- c("no", "no", "yes", "no", "no")
+area_fill_select_3
+
+nav_values_remaining <- cbind(nav_values_remaining, area_fill_select_3)
+nav_values_remaining <- as_data_frame(nav_values_remaining)
+
+ggplot(nav_values_remaining, 
+       aes(x = time_elapse, 
+           y = prop_etfs_remaining,
+           fill = area_fill_select_3)) +
+  geom_bar(stat = "identity",
+           colour = "#000000") +
+  labs(title = "Proportion of ETF Data Available by Years of Measurement",
+       x = "Years", 
+       y = "Proportion of ETFs \n without Missing Information") +
+  scale_x_discrete(labels = c("Since January", "1", "3", "5", "10")) +
+  scale_fill_manual(values = c("no" = "#ABDDDE", 
+                               "yes" = "#ECCBAE"),
+                    name = "Choice")
+ggsave("results/ishares_NAV_3_year.png")
