cce393220b1c456ca4baf0c38d05eed941236d80
danraff <d.raff@alumni.ubc.ca>
2017-11-23 18:22:36 -0800

    Improve description with reason for script

--- src/ishares_metadata_NAV_selection.R
+++ src/ishares_metadata_NAV_selection.R
@@ -6,6 +6,11 @@
 # and creates a dataframe comparing how much NAV data is lost with increasing 
 # length of time that the monthly NAV spans (eg: Year to Date vs. 1 Year, vs. 10 years)
 
+# The greater the length of time each NAV spans the better, 5 years will be more 
+# accurate than 1 year of the overall NAV (think of this as return) of the ETF. 
+# There are more missing values with a greater time span as well. 
+# This script produces a table comparing the amount of data lost (missing values)
+
 # Usage: Rscript ishares_metadata_NAV_selection.R
 
 library(tidyverse)
