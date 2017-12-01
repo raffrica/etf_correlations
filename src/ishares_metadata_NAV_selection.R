312a8668e11a2c689d2b9ace20781b4f69f74911
danraff <d.raff@alumni.ubc.ca>
2017-12-01 12:55:27 -0800

    Update with cmd line args + main()

--- src/ishares_metadata_NAV_selection.R
+++ src/ishares_metadata_NAV_selection.R
@@ -11,26 +11,37 @@
 # There are more missing values with a greater time span as well. 
 # This script produces a table comparing the amount of data lost (missing values)
 
-# Usage: Rscript ishares_metadata_NAV_selection.R
+# Usage: Rscript src/ishares_metadata_NAV_selection.R results/etf_metadata_ishares_clean.csv results/ishares_nav_values_not_missing.csv
 
-library(tidyverse)
-library(forcats)
-
-ishares <- read_csv("results/etf_metadata_ishares_clean.csv")
-
-names(ishares)
-
-ishares_nav_values_not_missing <- tibble(time_elapse = c("total", "year_to_date",
-"1_year", "3_years", "5_years", "10_years"),
-       num_etfs_remaining = map_dbl(names(ishares), 
-                                    function (x) sum(!is.na(ishares[[x]]))),
-       prop_etfs_remaining = map_dbl(names(ishares), 
-                                     function (x){
-                                       sum(!is.na(ishares[[x]]))/
-                                         sum(!is.na(ishares[[1]]))
-                                  }))
-ishares_nav_values_not_missing$time_elapse <- factor(ishares_nav_values_not_missing$time_elapse)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(forcats))
 
 
+# Read in Command Line Arguments
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- as.character(args[1])
+output_file <- as.character(args[2])
 
-write_csv(ishares_nav_values_not_missing, "results/ishares_nav_values_not_missing.csv")
+main <- function(){
+  
+  ishares <- read_csv(input_file)
+  
+  names(ishares)
+  
+  ishares_nav_values_not_missing <- tibble(time_elapse = c("total", "year_to_date",
+  "1_year", "3_years", "5_years", "10_years"),
+         num_etfs_remaining = map_dbl(names(ishares), 
+                                      function (x) sum(!is.na(ishares[[x]]))),
+         prop_etfs_remaining = map_dbl(names(ishares), 
+                                       function (x){
+                                         sum(!is.na(ishares[[x]]))/
+                                           sum(!is.na(ishares[[1]]))
+                                    }))
+  ishares_nav_values_not_missing$time_elapse <- factor(ishares_nav_values_not_missing$time_elapse)
+  
+  
+  
+  write_csv(ishares_nav_values_not_missing, output_file)
+}
+
+main()
