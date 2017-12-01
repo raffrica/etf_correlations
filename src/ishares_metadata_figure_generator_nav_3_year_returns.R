feae259dba27bb352799b340c4850825127a3525
danraff <d.raff@alumni.ubc.ca>
2017-12-01 13:07:31 -0800

    Include cmd line args + main()

--- src/ishares_metadata_figure_generator_nav_3_year_returns.R
+++ src/ishares_metadata_figure_generator_nav_3_year_returns.R
@@ -5,46 +5,53 @@
 # This script reads in clean ishares metadata and then looks generates
 # a histogram of the distribution of 3 year NAVs. 
 
-# Usage: Rscript ishares_metadata_figure_generator_nav_3_year_returns.R
+# Usage: Rscript src/ishares_metadata_figure_generator_nav_3_year_returns.R results/etf_metadata_ishares_clean.csv results/ishares_hist_etfs.png
 
-library(tidyverse)
-library(forcats)
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(forcats))
 
-# Read in clear ishares metadata
-ishares <- read_csv("results/etf_metadata_ishares_clean.csv")
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- args[1]
+output_figure <- args[2]
 
+main <- function(){
+  # Read in clear ishares metadata
+  ishares <- read_csv(input_file)
+  
+  
+  # From analysis generated in src/ishares_metadata_figure_generator_nav_values_not_missing.R
+  # choosing nav_monthly_3_year as the returns to look at.
+  ishares <- ishares %>% select(Ticker, nav_monthly_3_year)
+  
+  # Now to exclude all the missing data (i.e. exclude ETFs that don't have data
+  # from all of the last 3 years)  
+  ishares <- ishares[!is.na(ishares$nav_monthly_3_year),]
+  
+  ishares <- ishares %>% mutate(colour = factor(ifelse(nav_monthly_3_year == 10.72,
+                                     "S & P 500",
+                                     "Others")))
+  
+  ishares$colour <- fct_inorder(ishares$colour)
+  
+  # Histogram of the NAVs monthly over past 3 years
+  # should add something to highlight bin with S&P 500
+  ggplot(ishares, 
+         aes(x = nav_monthly_3_year,
+             fill = colour),
+         colour = "black") +
+    geom_histogram(bins = 30) +
+    labs(x = "Monthly NAV over past 3 Years", 
+         y = "Count", 
+         title = "Histogram of ETFs based on NAV data",
+         legend = "S & P 500") + 
+    scale_fill_manual(values = c("S & P 500" = "#ECCBAE", 
+                                 "Others" = "#ABDDDE"),
+                      name = "")
+  
+  ggsave(output_figure)
+}
 
-# From analysis generated in src/ishares_metadata_figure_generator_nav_values_not_missing.R
-# choosing nav_monthly_3_year as the returns to look at.
-ishares <- ishares %>% select(Ticker, nav_monthly_3_year)
-
-# Now to exclude all the missing data (i.e. exclude ETFs that don't have data
-# from all of the last 3 years)  
-ishares <- ishares[!is.na(ishares$nav_monthly_3_year),]
-
-ishares <- ishares %>% mutate(colour = factor(ifelse(nav_monthly_3_year == 10.72,
-                                   "S & P 500",
-                                   "Others")))
-
-ishares$colour <- fct_inorder(ishares$colour)
-
-# Histogram of the NAVs monthly over past 3 years
-# should add something to highlight bin with S&P 500
-ggplot(ishares, 
-       aes(x = nav_monthly_3_year,
-           fill = colour),
-       colour = "black") +
-  geom_histogram() +
-  labs(x = "Monthly NAV over past 3 Years", 
-       y = "Count", 
-       title = "Histogram of ETFs based on NAV data",
-       legend = "S & P 500") + 
-  scale_fill_manual(values = c("S & P 500" = "#ECCBAE", 
-                               "Others" = "#ABDDDE"),
-                    name = "")
-
-ggsave("results/ishares_hist_etfs.png")
-
+main()
 
 
 
